e939f643b53b08d9c77f2b1f77be3c1c114f5959
Nikolai Schumacher <nikolai@Nikolais-MacBook-Pro.local>
2022-04-21 20:15:23 +0200

    stuff

--- srcs/requirements/mariadb/config.sql
+++ srcs/requirements/mariadb/config.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE wordpress IF NOT EXISTS wordpress;
+CREATE DATABASE wordpress;
 /* Should be admin has all Privileges */
-CREATE USER IF NOT EXISTS 'nschumac'@'%' IDENTIFIED BY '123';
+CREATE USER 'nschumac'@'%' IDENTIFIED BY '123';
 
 /* Should be normal User has no Privileges */
 CREATE USER 'nschumac2'@'%' IDENTIFIED BY '1234';
@@ -9,4 +9,4 @@ CREATE USER 'nschumac2'@'%' IDENTIFIED BY '1234';
 adds admin privileges to user nschumac
 GRANT flush and reloads everything 
 */
-GRANT ALL ON wordpress.* TO 'nschumac'@'%' WITH GRANT OPTION;
+GRANT ALL ON wordpress.* TO 'nschumac'@'%';
